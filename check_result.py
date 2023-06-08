af412a79776ef52f2b8d5c8f68085bf8f9abc1cc
User <user@admins-MacBook-Pro.local>
2023-06-08 16:54:02 +0700

    add simple auth to api

--- check_result.py
+++ check_result.py
@@ -1,12 +1,25 @@
 import base64
 from dotenv import load_dotenv
-from fastapi import FastAPI,status
+from fastapi import FastAPI, HTTPException, status, Security
 from fastapi.responses import JSONResponse , Response
+from fastapi.security import APIKeyHeader
 from pydantic import BaseModel
 import pymongo
 import os
 
 load_dotenv()
+API_KEYS = eval(os.getenv("API_KEYS"))
+
+api_key_header = APIKeyHeader(name="x-api-key", auto_error=False)
+def get_api_key(
+    api_key_header: str = Security(api_key_header),
+) -> str:
+    if api_key_header in API_KEYS:
+        return api_key_header
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid or missing API Key",
+    )
 
 mongo_db = pymongo.MongoClient(os.getenv("MONGO_HOST"))
 
@@ -17,10 +30,10 @@ class Body(BaseModel):
 
 @app.get("/healthcheck")
 def healthcheck():
-    return JSONResponse(status_code=status.HTTP_200_OK)
+    return JSONResponse(status_code=status.HTTP_200_OK,content="Ok")
 
 @app.post("/get")
-def get_job_result(body : Body ) :
+def get_job_result(body : Body, api_key: str = Security(get_api_key) ) :
     job_id = body.job_id
 
     result = [elem for elem in mongo_db["jobs"]["grayscale-images"].find({"job_id":job_id})]
@@ -31,8 +44,7 @@ def get_job_result(body : Body ) :
 
     else :
         image_bytes = base64.b64decode(result["image"])
-        response = Response(status_code=status.HTTP_200_OK,)
+        response = Response(status_code=status.HTTP_200_OK,content=image_bytes)
         response.headers["Content-Type"] = "image/png"  
         response.headers["Content-Disposition"] = "attachment; filename=image.png" 
-        response.content = image_bytes
         return response
--- submit_job.py
+++ submit_job.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
-from fastapi import FastAPI,status
+from fastapi import FastAPI, HTTPException, status, Security
 from fastapi.responses import JSONResponse
+from fastapi.security import APIKeyHeader
+from pydantic import BaseModel
 import json
 from pika import (
     BasicProperties,
@@ -11,6 +13,18 @@ from pydantic import BaseModel
 import os
 
 load_dotenv()
+API_KEYS = eval(os.getenv("API_KEYS"))
+
+api_key_header = APIKeyHeader(name="x-api-key", auto_error=False)
+def get_api_key(
+    api_key_header: str = Security(api_key_header),
+) -> str:
+    if api_key_header in API_KEYS:
+        return api_key_header
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid or missing API Key",
+    )
 
 app =FastAPI()
 
@@ -18,7 +32,7 @@ class Body(BaseModel):
     url : str
 
 RABBITMQ_HOST = os.getenv("RABBITMQ_HOST")
-RABBITMQ_PORT = os.getenv("RABBITMQ_PORT")
+RABBITMQ_PORT = int(os.getenv("RABBITMQ_PORT"))
 IMAGE_QUEUE_NAME = os.getenv("RABBITMQ_IMAGE_QUEUE")
 
 @app.get("/healthcheck")
@@ -26,7 +40,7 @@ def healthcheck():
     return JSONResponse(status_code=status.HTTP_200_OK)
 
 @app.post("/convert")
-def get_job_result(body : Body ) :
+def get_job_result(body : Body, api_key: str = Security(get_api_key)) :
     
     message = {"url": body.url, "job_id":hash(body.url)}
 
