a0f9d49237082d19cb5356bbb9ce49e351184c4a
User <user@admins-MacBook-Pro.local>
2023-06-09 12:10:45 +0700

    seperate mongo and rabbit function

--- check_result.py
+++ check_result.py
@@ -1,16 +1,19 @@
 import base64
+from typing import List
+import os
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException, status, Security
-from fastapi.responses import JSONResponse , Response
+from fastapi.responses import JSONResponse, Response
 from fastapi.security import APIKeyHeader
 from pydantic import BaseModel
 import pymongo
-import os
 
 load_dotenv()
-API_KEYS = eval(os.getenv("API_KEYS"))
+API_KEYS = eval(os.getenv("API_KEYS"))  # pylint: disable=eval-used
 
 api_key_header = APIKeyHeader(name="x-api-key", auto_error=False)
+
+
 def get_api_key(
     api_key_header: str = Security(api_key_header),
 ) -> str:
@@ -21,30 +24,59 @@ def get_api_key(
         detail="Invalid or missing API Key",
     )
 
+
+MONGO_DB_NAME = os.getenv("DB_NAME")
+MONGO_COL_NAME = os.getenv("COL_NAME")
+
 mongo_db = pymongo.MongoClient(os.getenv("MONGO_HOST"))
 
-app =FastAPI()
+
+def mongo_query(
+    db_name: str = MONGO_DB_NAME,
+    col_name: str = MONGO_COL_NAME,
+    query: object = {},
+) -> List:
+    result = mongo_db[db_name][col_name].find(query)
+
+    return [obj for obj in result]
+
+
+def b64decode(b64img: str):
+    return base64.b64decode(b64img)
+
+
+app = FastAPI()
+
 
 class Body(BaseModel):
-    job_id : int
+    job_id: int
+
 
 @app.get("/healthcheck")
 def healthcheck():
-    return JSONResponse(status_code=status.HTTP_200_OK,content="Ok")
+    return JSONResponse(status_code=status.HTTP_200_OK, content="Ok")
+
 
 @app.post("/get")
-def get_job_result(body : Body, api_key: str = Security(get_api_key) ) :
+def get_job_result(body: Body, api_key: str = Security(get_api_key)):
     job_id = body.job_id
 
-    result = [elem for elem in mongo_db["jobs"]["grayscale-images"].find({"job_id":job_id})]
-    if len(result) == 0 :
+    result = [
+        elem
+        for elem in mongo_query(
+            db_name=MONGO_DB_NAME, col_name=MONGO_COL_NAME, query={"job_id": job_id}
+        )
+    ]
+    if len(result) == 0:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={
+                "message": "No such result with job_id (The job may being processed or no job with this id was submitted)"
+            },
+        )
 
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,content={
-            "message":"No such result with job_id (The job may being processed or no job with this id was submitted)"})
-
-    else :
-        image_bytes = base64.b64decode(result["image"])
-        response = Response(status_code=status.HTTP_200_OK,content=image_bytes)
-        response.headers["Content-Type"] = "image/png"  
-        response.headers["Content-Disposition"] = "attachment; filename=image.png" 
-        return response
+    image_bytes = b64decode(result["image"])
+    response = Response(status_code=status.HTTP_200_OK, content=image_bytes)
+    response.headers["Content-Type"] = "image/png"
+    response.headers["Content-Disposition"] = "attachment; filename=image.png"
+    return response
--- submit_job.py
+++ submit_job.py
@@ -1,21 +1,18 @@
+import json
+import os
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException, status, Security
 from fastapi.responses import JSONResponse
 from fastapi.security import APIKeyHeader
+from pika import BasicProperties, BlockingConnection, ConnectionParameters, spec
 from pydantic import BaseModel
-import json
-from pika import (
-    BasicProperties,
-    BlockingConnection, 
-    ConnectionParameters, 
-    spec)
-from pydantic import BaseModel
-import os
 
 load_dotenv()
 API_KEYS = eval(os.getenv("API_KEYS"))
 
 api_key_header = APIKeyHeader(name="x-api-key", auto_error=False)
+
+
 def get_api_key(
     api_key_header: str = Security(api_key_header),
 ) -> str:
@@ -26,32 +23,61 @@ def get_api_key(
         detail="Invalid or missing API Key",
     )
 
-app =FastAPI()
+
+app = FastAPI()
+
 
 class Body(BaseModel):
-    url : str
+    url: str
+
 
 RABBITMQ_HOST = os.getenv("RABBITMQ_HOST")
 RABBITMQ_PORT = int(os.getenv("RABBITMQ_PORT"))
 IMAGE_QUEUE_NAME = os.getenv("RABBITMQ_IMAGE_QUEUE")
 
+
+def rabbitmq_publish(
+    message,
+    host: str = RABBITMQ_HOST,
+    port: str = RABBITMQ_PORT,
+    queue: str = IMAGE_QUEUE_NAME,
+):
+    try:
+        mq_connection = BlockingConnection(ConnectionParameters(host=host, port=port))
+        mq_channel = mq_connection.channel()
+
+        mq_channel.queue_declare(queue=queue, durable=True)
+        mq_channel.basic_publish(
+            exchange="",
+            routing_key=queue,
+            body=message,
+            properties=BasicProperties(delivery_mode=spec.PERSISTENT_DELIVERY_MODE),
+        )
+        mq_connection.close()
+
+        return True
+
+    except:
+        return False
+
+
 @app.get("/healthcheck")
 def healthcheck():
-    return JSONResponse(status_code=status.HTTP_200_OK)
+    return JSONResponse(status_code=status.HTTP_200_OK, content="Ok")
+
 
 @app.post("/convert")
-def get_job_result(body : Body, api_key: str = Security(get_api_key)) :
-    
-    message = {"url": body.url, "job_id":hash(body.url)}
+def get_job_result(body: Body, api_key: str = Security(get_api_key)):
+    message = {"url": body.url, "job_id": hash(body.url)}
 
-    mq_connection = BlockingConnection(ConnectionParameters(host=RABBITMQ_HOST,port = RABBITMQ_PORT))
-    mq_channel = mq_connection.channel()
+    message_sent = rabbitmq_publish(json.dumps(message))
 
-    mq_channel.queue_declare(queue=IMAGE_QUEUE_NAME,durable=True)
-    mq_channel.basic_publish(exchange="",routing_key=IMAGE_QUEUE_NAME,
-                                body=json.dumps(message),
-                                properties=BasicProperties(delivery_mode=spec.PERSISTENT_DELIVERY_MODE))
-    mq_connection.close()
+    if message_sent:
+        return JSONResponse(
+            status_code=status.HTTP_200_OK, content={"message": "Job Submitted"}
+        )
 
-    return JSONResponse(status_code=status.HTTP_200_OK,content={
-        "message":"Job Submitted"})
+    return JSONResponse(
+        status_code=status.HTTP_500_OK,
+        content={"message": "Error occured with rabbitmq"},
+    )
